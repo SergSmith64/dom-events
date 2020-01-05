226dc1f773f8491c2f11b5dbea86a8276d943e52
SergSmith64 <CheapThailand@gmail.com>
2020-01-06 00:21:59 +0300

    feat: add events

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log("DOM-Events___");
+let step = 0;
+const infoLogger = () => {
+    step++;
+    console.log(`step - ${step}`, event.type);
+}
+
+document.querySelector('li').addEventListener('click', infoLogger);
+// document.querySelector('li').addEventListener('dblclick', infoLogger);
+
+// Right Key Click
+// document.querySelector('li').addEventListener('contextmenu', infoLogger);  
+// document.querySelector('li').addEventListener('mouseover', infoLogger);
+// document.querySelector('li').addEventListener('mouseout', infoLogger);
+// document.querySelector('li').addEventListener('mousedown', infoLogger);
+// document.querySelector('li').addEventListener('mouseup', infoLogger);
+// document.querySelector('li').addEventListener('click', infoLogger);
+// document.querySelector('li').addEventListener('click', infoLogger);
+
+// document.querySelector('li').addEventListener('click', infoLogger);
+// document.querySelector('li').addEventListener('copy', () => {
+//     console.log("Ha-Ha!!");
+//     document.querySelector('li').removeEventListener('click', infoLogger);
+// });
+
+
+
+// document.querySelector('li').removeEventListener('click', () => {
+//     console.log("action-2");
+// })
